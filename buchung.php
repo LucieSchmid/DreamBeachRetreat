9ba159f48a948ffe72032145939a3dc1d027e439
corinna04 <wallner.corinna@hakspittal.at>
2024-01-31 18:07:12 +0100

    Gewinn der Verzweiflung

--- buchung.php
+++ buchung.php
@@ -24,11 +24,11 @@
             || empty($_POST['verpflegung']) || empty($_POST['anreiseDatum']) || empty($_POST['anreiseZeit'])
             || empty($_POST['abreiseDatum']) || empty($_POST['abreiseZeit'])
         ) {
-        //Wenn ja dass den Benutzer melden
+            //Wenn ja dass den Benutzer melden
             echo "Bitte geben Sie die nötigen Daten ein. Umso mehr wir über ihre Wünsche wissen, desto besser können wir
             ihren Urlaub planen.";
         } else {
-        //Wenn alles da ist weitermachen
+            //Wenn alles da ist weitermachen
             //auspacken
             $nachname = trim(htmlspecialchars($_POST['nachname']));
             $vorname = trim(htmlspecialchars($_POST['vorname']));
@@ -43,77 +43,79 @@
             $abreise = trim(htmlspecialchars($_POST['abreiseDatum'])) . " " . trim(htmlspecialchars($_POST['abreiseZeit']));
 
             //Anfang des Daten kontrollieren
-           if(isset($_POST['bestaetigen'])){
-            if(empty($_POST['sicher'])){
-                    echo"Bitte lassen Sie uns über die Richtigkeit ihrer Daten wissen";
-            }else{
-                $sicher=$_POST['sicher'];
-                if($sicher == "nein"){
-                    echo"Lassen Sie uns wissen welche Informationen falsch sind.";
-                }else{
-                    //DB Verbindung
-            require_once('db.php');
+            if (isset($_POST['bestaetigen'])) {
+                if (empty($_POST['sicher'])) {
+                    echo "Bitte lassen Sie uns über die Richtigkeit ihrer Daten wissen";
+                } else {
+                    $sicher = $_POST['sicher'];
+                    if ($sicher == "ja") {
+                        //DB Verbindung
+                        require_once('db.php');
 
-            //SQL-Statement aufbauen
-            try{   
-                $statement = $pdo->prepare("INSERT INTO buchung(nachname, vorname, anschrift, ort, telefon, wieAlt, zimmer,
-                personenProZimmer,anreise, abreise, verpflegung) 
-                VALUES (:nachname, :vorname, :anschrift, :ort, :telefon, :wieAlt, :zimmer, :personenProZimmer, :anreise, :abreise, :verpflegung)");
-                //alle PLatzhalter mit Werten belegen (binden)
-                $statement->bindParam(":nachname", $nachname);
-                $statement->bindParam(":vorname", $vorname);
-                $statement->bindParam(":anschrift", $anschrift);
-                $statement->bindParam(":ort", $ort);
-                $statement->bindParam(":telefon", $tel);
-                $statement->bindParam(":wieAlt", $alter);
-                $statement->bindParam(":zimmer", $zimmerAnz);
-                $statement->bindParam(":personenProZimmer", $personenAnz);
-                $statement->bindParam(":anreise", $anreise);
-                $statement->bindParam(":abreise", $abreise);
-                $statement->bindParam(":verpflegung", $verpflegung);
+                        //SQL-Statement aufbauen
+                        try {
+                            $statement = $pdo->prepare("INSERT INTO buchung(nachname, vorname, anschrift, ort, telefon, wieAlt, zimmer,
+                                       personenProZimmer,anreise, abreise, verpflegung) 
+                                       VALUES (:nachname, :vorname, :anschrift, :ort, :telefon, :wieAlt, :zimmer, :personenProZimmer, :anreise, :abreise, :verpflegung)");
+                            //alle PLatzhalter mit Werten belegen (binden)
+                            $statement->bindParam(":nachname", $nachname);
+                            $statement->bindParam(":vorname", $vorname);
+                            $statement->bindParam(":anschrift", $anschrift);
+                            $statement->bindParam(":ort", $ort);
+                            $statement->bindParam(":telefon", $tel);
+                            $statement->bindParam(":wieAlt", $alter);
+                            $statement->bindParam(":zimmer", $zimmerAnz);
+                            $statement->bindParam(":personenProZimmer", $personenAnz);
+                            $statement->bindParam(":anreise", $anreise);
+                            $statement->bindParam(":abreise", $abreise);
+                            $statement->bindParam(":verpflegung", $verpflegung);
 
-                //Statement ausführen
-               $statement-> execute();
-
-               }catch(PDOException $e){
-                   echo $e->getMessage();
-               die("Fehler beim buchen.");
-           }
+                            //Statement ausführen
+                            $statement->execute();
+                        } catch (PDOException $e) {
+                            echo $e->getMessage();
+                            die("Fehler beim buchen.");
+                        }
+                        echo "<h2>Vielen Dank für das bestätigen ihrer Daten. Ihre Buchung konnte erfolgreich durchgeführt werden.
+                                           Hier wartet alles nurmehr auf sie!</h2>";
+                    } else {
+                        //Kunden dass Buchungsformular nochmal neu anzeigen mit entsprechender Meldung
+                    }
                 }
-            }
-       } else{
-                ?>
-            <form  method="post">
-            <h2> Vielen Dank für ihre Buchung!<br>Wir freuen uns sehr dass sie sich für einen Urlaub
-            bei Dream Beach Retreat entschieden haben.</h2>
-            Bitte kontrollieren Sie nochamls folgende Daten auf ihre Richtigkeit.<br>Dies ist ein essentieler
-            Schritt für die reibungslose Abwicklung ihrer Urlaubsbuchung:
-            <?php
-            echo"Ihr Zuname lautet: <b>$nachname</b><br>";
-            echo"Ihr Vorname lautet: <b>$vorname</b><br>";
-            echo"Ihre Anschrift lautet: <b>$anschrift</b><br>";
-            echo"Ihr Wohnort lautet: <b>$ort</b><br>";
-            echo"Ihre Telefonnummer lautet: <b>$tel</b><br>";
-            echo"Sie sind <b>$alter</b> Jahre alt<br>";
-            echo"Sie wollen <b>$zimmerAnz</b> Zimmer buchen. Welche jeweils <b>$personenAnz</b> beherbergen<br>";
-            echo"Sie wünschen sich eine <b>$verpflegung</b><br>";
-            echo"Ihr Urlaub beginnt: <b>$anreise</b><br>";
-            echo"Ihr Urlaub endet: <b>$abreise</b><br>";
-            ?>
-            <input type="radio" name="sicher" value="ja"> Ja, folgende Daten sind richtig
-            <br>
-            <input type="radio" name="sicher" value="nein"> Nein, die folgenden Daten sind nicht richtig
-            <br>
-            <br>
-            <input type="submit" name="bestaetigen" value="bestätigen">
-            </form>
+            } else {
+    ?>
+                <form enctype="multipart/form-data" method="post" action="<?php echo $_SERVER["SCRIPT_NAME"]; ?>">
+                    <h2> Vielen Dank für ihre Buchung!<br>Wir freuen uns sehr dass sie sich für einen Urlaub
+                        bei Dream Beach Retreat entschieden haben.</h2>
+                    Bitte kontrollieren Sie nochamls folgende Daten auf ihre Richtigkeit.<br>Dies ist ein essentieler
+                    Schritt für die reibungslose Abwicklung ihrer Urlaubsbuchung:
+                    <br>
+                    <?php
+                    echo "Ihr Zuname lautet: <b>$nachname</b><br>";
+                    echo "Ihr Vorname lautet: <b>$vorname</b><br>";
+                    echo "Ihre Anschrift lautet: <b>$anschrift</b><br>";
+                    echo "Ihr Wohnort lautet: <b>$ort</b><br>";
+                    echo "Ihre Telefonnummer lautet: <b>$tel</b><br>";
+                    echo "Sie sind <b>$alter</b> Jahre alt<br>";
+                    echo "Sie wollen <b>$zimmerAnz</b> Zimmer buchen. Welche jeweils <b>$personenAnz</b> beherbergen<br>";
+                    echo "Sie wünschen sich eine <b>$verpflegung</b><br>";
+                    echo "Ihr Urlaub beginnt: <b>$anreise</b><br>";
+                    echo "Ihr Urlaub endet: <b>$abreise</b><br>";
+                    ?>
+                    <input type="radio" name="sicher" value="ja"> Ja, folgende Daten sind richtig
+                    <br>
+                    <input type="radio" name="sicher" value="nein"> Nein, die folgenden Daten sind nicht richtig
+                    <br>
+                    <br>
+                    <input type="submit" name="bestaetigen" value="bestätigen">
+                </form>
         <?php
             }
-            
-//echtes Formular
+
+            //echtes Formular
         }
     } else {
-    ?>
+        ?>
         <h3>
             Buchen Sie jetzt! Sie sind nur noch wenige Mausklicks davon entfernt,
             den Urlaub Ihrer Träume zu erleben. Damit wir Ihren Aufenthalt so traumhaft wie möglich gestalten können,
@@ -159,7 +161,7 @@
                     <td><label for="verpflegung">Welche Art der Verpflegung wünschen Sie?</label></td>
                     <td><input type="radio" id="verpflegung" name="verpflegung" value="vollpension" required> Vollpension
                         <br>
-                        <input type="radio" id="verpflegung" name="verpflegung" value="Halbpension" required> Halbpension
+                        <input type="radio" id="verpflegung" name="verpflegung" value="halbpension" required> Halbpension
                     </td>
                 </tr>
                 <tr>
