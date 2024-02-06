00848a8fd526e767df3d296f254b053e57a2902f
corinna04 <wallner.corinna@hakspittal.at>
2024-02-06 08:29:20 +0100

    funkt ertsmol olls

--- ausgabe.php
+++ ausgabe.php
@@ -9,6 +9,7 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ausgabe</title>
+    <link rel="stylesheet" type="text/css" href="stand.css">
 </head>
 
 <body>
