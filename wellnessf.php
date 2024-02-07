6e3dbc60e2e44c1b5397bf4b7026f586cd4add79
rytchs <reinwaldangelina@gmail.com>
2024-02-07 13:52:49 +0100

    wellness formular funkt mehr oder weniger

--- wellnessf.php
+++ wellnessf.php
@@ -13,17 +13,17 @@ session_start();
 </head>
 
 <body>
-<header>
-<h1>
-        <div style="float:left; padding:5px;"> <img class="runde-ecken" src="images/logo.png" width="100" height="100" /></div>
-        Dream Beach Retreat
-    </h1>
-</header>
+    <header>
+        <h1>
+            <div style="float:left; padding:5px;"> <img class="runde-ecken" src="images/logo.png" width="100" height="100" /></div>
+            Dream Beach Retreat
+        </h1>
+    </header>
 
     <?php
 
     $art;
-
+    $preis;
 
     if (!isset($_SESSION['email'])) {
         echo "Sie müssen sich zuerst anmelden. <a href='login.php'>Hier geht es zum Login.</a>";
@@ -36,56 +36,67 @@ session_start();
                 $art = $_POST['art'];
                 $email = $_SESSION['email'];
 
-                echo "<h1>Dream Beach Retreat - Wellnessbuchung</h1>";
+                echo "<h2>Dream Beach Retreat - Wellnessbuchung</h2>";
                 echo "<p>Vielen Dank für Ihre Buchung!</p><br>";
 
 
-                require_once('db.php'); 
-
-                
-
-
-
                 echo $_SESSION['email'] . "<br>";
-                echo $art;
+                echo $art . "<br>";
+
+                require_once('db.php');
 
 
+                //Ausgeben der Daten aus der anderen Datenbank
                 try {
-
-                    $statement = $pdo->prepare("SELECT preis FROM wellnessf WHERE art = $art");
-
-                    $statement = $pdo->prepare("INSERT INTO sportb (email, art) VALUES (:email, :art)");
-    
-                    $statement->bindParam(':email', $email);
+                    $statement = $pdo->prepare("SELECT preis, uhrzeit FROM wellnessf WHERE art = :art");
                     $statement->bindParam(':art', $art);
-    
                     $statement->execute();
-                } catch (PDOException $ex) {
-                    die("Fehler beim Einfügen der Daten in die Datenbank!");
+
+                    $row = $statement->fetch(PDO::FETCH_ASSOC);
+                    $preis = $row['preis'];
+                    $uhrzeit = $row['uhrzeit'];
+
+                    echo $preis . "€ <br>";
+                    echo $uhrzeit . " Uhr <br>";
+
+                    //Daten eintragen in die Datenbank
+                    try {
+
+                        $st = $pdo->prepare("INSERT INTO wellnessb (email, art, preis, uhrzeit) VALUES (:email, :art, :preis, :uhrzeit)");
+
+                        $st->bindParam(':email', $email);
+                        $st->bindParam(':art', $art);
+                        $st->bindParam(':preis', $preis);
+                        $st->bindParam(':uhrzeit', $uhrzeit);
+
+                        $st->execute();
+                    } catch (PDOException $ex) {
+                        die("Fehler beim Eintragen der Daten in die Datenbank.");
+                    }
+                } catch (PDOException $e) {
+                    die("Fehler beim Ausgeben der Daten.");
                 }
-
-
             }
         } else {
     ?>
 
 
 
-        <h2>Wellness-Angebote</h2>
-        <br>
-        <p>Hinweis: Die Wellnessangebote gibt es an egal welchen Wochentag zur Verfügung.</p>
-        <form method="POST" action="<?php echo $_SERVER['SCRIPT_NAME']; ?>">
+            <h2>Wellness-Angebote</h2>
+            <br>
+            <p>Hinweis: Die Wellnessangebote gibt es an egal welchen Wochentag zur Verfügung.</p>
+            <form method="POST" action="<?php echo $_SERVER['SCRIPT_NAME']; ?>">
 
-            <label>Wellness-Art:</label><br>
-            <input type="radio" name="art" value="Massage (alles)">Massage (alles)<br>Preis: 45,00€ <br>Uhrzeit: 19:00 Uhr<br><br>
-            <input type="radio" name="art" value="Tai-Massage" checked>Tai-Massage <br>Preis: 30,00€<br>Uhrzeit: 20:00 Uhr<br><br>
-            <input type="radio" name="art" value="Rückenmassage">Rückenmassage <br>Preis: 25,00€<br>Uhrzeit: 21:00 Uhr<br><br>
-            <input type="radio" name="art" value="Fußmassage"> Fußmassage<br> Preis: 25,00€<br>Uhrzeit: 20:30 Uhr
-            <br><br>
+                <label>Wellness-Art:</label><br>
+                <input type="radio" name="art" value="Massage (alles)">Massage (alles)<br>Preis: 45,00€ <br>Uhrzeit: 19:00 Uhr<br><br>
+                <input type="radio" name="art" value="Tai-Massage" checked>Tai-Massage <br>Preis: 30,00€<br>Uhrzeit: 20:00 Uhr<br><br>
+                <input type="radio" name="art" value="Rückenmassage">Rückenmassage <br>Preis: 25,00€<br>Uhrzeit: 21:00 Uhr<br><br>
+                <input type="radio" name="art" value="Fußmassage"> Fußmassage<br> Preis: 25,00€<br>Uhrzeit: 20:30 Uhr
+                <br><br>
 
-            <input type="submit" value="Buchen" name="buchen">
+                <input type="submit" value="Buchen" name="buchen">
 
-        </form>
+            </form>
 
 
     <?php
