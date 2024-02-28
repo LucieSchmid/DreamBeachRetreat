3d22aec2589aac8518d71374724ce9cdb270bc5f
LucieSchmid <schmid.lucie@hakspittal.at>
2024-02-28 14:40:04 +0100

    Login

--- login.php
+++ login.php
@@ -11,15 +11,28 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Anmeldung</title>
-    <link rel="stylesheet" type="text/css" href="standf.css">
-</head>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+
+    <script src="https://kit.fontawesome.com/1456f04639.js" crossorigin="anonymous"></script>
+
+    <link rel="stylesheet" type="text/css" href="login.css">
+
 
 <body>
     <header>
-        <h1>
-            <div style="float:left; padding:5px;"> <img class="runde-ecken" src="images/logo.png" width="150" height="150" /></div>
-            Dream Beach Retreat
-        </h1>
+        <!-- <h1>
+            <div style="float:left; padding-top:5px; margin: 0px;"><a href="hauptseite.html"> <img class="runde-ecken" src="images/logo.png" /></a></div>
+            Dream Beach Retreat</h1>
+            <h2 style="padding-top: 0px; margin: 0px;">Login</h2>
+        -->
+        <li class="hintergrund" style="display: grid; grid-template-columns: 100px ; grid-template-rows: 50px 50px; padding-bottom: 10px; ">
+            <a href="hauptseite.html"><img src="images/logo.png" width="60" height="60" alt="Logo" class="img-fluid rounded m-4" aligen="left" style="grid-row: 1 / 2"></a>
+            <h1 style="grid-row: 1; grid-column: 2; text-align: left; margin-top: 20px;">Dream Beach Retreat</h1>
+            <i style="grid-row: 2 ; grid-column: 2; text-align: left; margin-top: 10px; padding-top: 4px;">Entspannen, Auftanken und Genießen - Dein Rückzugsort am Strand.</i>
+        </li>
+
     </header>
     <?php
 
@@ -102,21 +115,39 @@ session_start();
         if ($formular_anzeigen) {
 
     ?>
-            <h2>Login</h2>
-            <br>
-            <form method="POST" action="<?php echo $_SERVER['SCRIPT_NAME']; ?>">
-                <p>Solltest Du noch nicht registriert sein, dann klicke <a href="registrieren.php">hier</a></p>
 
-                <h3>Anmeldedaten</h3>
-
-                <label for="email">E-Mail:</label>
-                <input type="email" id="email" name="email" value="" required><br>
-                <label for="passwort">Passwort:</label>
-                <input type="password" id="passwort" name="passwort" required><br><br>
-
-                <input type="submit" value="Login" name="login" class="button">
-            </form>
-
+            <div class="p-5 h-100 d-flex align-items-center justify-content-center">
+                <div class="p-5 border1">
+                    <div class="h-100 d-flex align-items-center justify-content-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
+                            <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6" />
+                        </svg>
+                    </div>
+                    <div class="h-100 d-flex align-items-center justify-content-center">
+                        <h3>Login</h3>
+                    </div>
+                    <form method="POST" action="<?php echo $_SERVER['SCRIPT_NAME']; ?>">
+                        <table>
+                            <div class="mb-3 mt-3">
+                                <tr>
+                                    <td><label for="email">E-Mail:</label></td>
+                                    <td><input type="email" id="email" name="email" value="" placeholder="Email eingeben" required></td>
+                                </tr>
+                            </div>
+                            <div class="mb-3">
+                                <tr>
+                                    <td><label for="passwort">Passwort:</label></td>
+                                    <td><input type="password" id="passwort" name="passwort" placeholder="Passwort eingeben" required></td>
+                                <tr>
+                            </div>
+                        </table>
+                        <div>
+                            <p class="mt-5">Noch kein Konto? Dann klicke <a href="registrieren.php">hier</a></p>
+                        </div>
+                        <input type="submit" value="Login" name="login" class="button">
+                    </form>
+                </div>
+            </div>
     <?php
         }
     }
