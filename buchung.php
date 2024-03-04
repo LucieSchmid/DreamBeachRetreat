2d089e429cc6ca071c27cf57fb1c93b4ec9089b3
LucieSchmid <schmid.lucie@hakspittal.at>
2024-03-04 11:22:41 +0100

    Zurückbutton

--- buchung.php
+++ buchung.php
@@ -9,11 +9,17 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+
+    <script src="https://kit.fontawesome.com/1456f04639.js" crossorigin="anonymous"></script>
+
     <link rel="stylesheet" type="text/css" href="standf.css">
     <title>Buchungen</title>
 </head>
 
 <body>
+    <i class="fas fa-arrow-left" data-bs-toggle="tooltip" data-bs-placement="right" title="Klicken Sie hier um zurückzukehren!" onclick="history.back()"></i>
     <?php
     if (!isset($_SESSION['email'])) {
         echo "Sie müssen sich zuerst anmelden. <a href='login.php'>Hier geht es zum Login.</a>";
--- wellnessf.php
+++ wellnessf.php
@@ -8,13 +8,19 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Wellnessformular</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+
+    <script src="https://kit.fontawesome.com/1456f04639.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" type="text/css" href="standf.css">
 </head>
 
 <body>
     <header>
         <h1>
+            <i class="fas fa-arrow-left" data-bs-toggle="tooltip" data-bs-placement="right" title="Klicken Sie hier um zurückzukehren!" onclick="history.back()"></i>
             <div style="float:left; padding:5px;"> <img class="runde-ecken" src="images/logo.png" width="150" height="150" /></div>
             Dream Beach Retreat
         </h1>
