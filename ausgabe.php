d0da2acd6b26536fdfaba977d51b6d08e4146d12
LucieSchmid <schmid.lucie@hakspittal.at>
2024-03-06 13:41:19 +0100

    dd

--- ausgabe.php
+++ ausgabe.php
@@ -10,9 +10,6 @@ session_start();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ausgabe</title>
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-
     <script src="https://kit.fontawesome.com/1456f04639.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" type="text/css" href="standf.css">
 </head>
