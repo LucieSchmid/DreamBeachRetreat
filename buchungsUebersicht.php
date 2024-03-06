25c95a959abebff4726b06d50da233473af33ffc
corinna04 <wallner.corinna@hakspittal.at>
2024-03-06 13:32:02 +0100

    file erstellt

---
+++ buchungsUebersicht.php
@@ -0,0 +1,19 @@
+<?php
+/*diese Seite dient asl Überischt für den Kunden
+hier kann er all seine Buchungen (Aufenthalt, Sport, Wellness)
+im Überblick behalten*/
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="standf.css">
+    <title>deine Buchungen</title>
+</head>
+<body>
+<?php
+?>
+
+</body>
+</html>
